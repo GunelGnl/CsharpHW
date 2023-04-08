2d9d994cd2bd5fa04fc11083407039efeafa4145
Gunel <kocharlig@gmail.com>
2023-04-08 17:37:50 +0400

    HW1

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Add firstnumber:");
+int a = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Add secondnumber:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a > b ? "firstnumber more than secondnumber" : "secondnumber more than firstnumber");
