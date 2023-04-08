76c51b33dc5c8b3e9253f8c0a9b1b7d0a5ac2507
Gunel <kocharlig@gmail.com>
2023-04-08 18:10:49 +0400

    2nd HW is done

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Add firstnumber:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Add secondnumber:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Add thirdnumber:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (a > b)
+{
+    max = a;
+}
+if (b > a)
+{
+    max = b;
+}
+if (c > a)
+{
+   max = c; 
+}
+Console.WriteLine("max = " + max);
