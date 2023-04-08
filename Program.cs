a1de6b2ac9dc901afdd195ab6b8884b41be4f069
Gunel <kocharlig@gmail.com>
2023-04-08 22:21:44 +0400

    Task3

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Add Number");
+int Number = Convert.ToInt32(Console.ReadLine());
+int divrem = Number/2;
+if (divrem == 0)
+{
+    Console.WriteLine ("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
